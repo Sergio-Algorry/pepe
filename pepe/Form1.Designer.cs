fd414671d96d99f0eccef8933109e5977a8dc076
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2023-08-15 13:53:05 -0300

    Agregar archivos de proyecto.

---
+++ pepe/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace pepe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIndicacion = new System.Windows.Forms.Label();
+            this.btAceptar = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblIndicacion
+            // 
+            this.lblIndicacion.AutoSize = true;
+            this.lblIndicacion.Location = new System.Drawing.Point(12, 35);
+            this.lblIndicacion.Name = "lblIndicacion";
+            this.lblIndicacion.Size = new System.Drawing.Size(186, 13);
+            this.lblIndicacion.TabIndex = 0;
+            this.lblIndicacion.Text = "Ingrese un número y presione Aceptar";
+            // 
+            // btAceptar
+            // 
+            this.btAceptar.Location = new System.Drawing.Point(118, 12);
+            this.btAceptar.Name = "btAceptar";
+            this.btAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btAceptar.TabIndex = 1;
+            this.btAceptar.Text = "Aceptar";
+            this.btAceptar.UseVisualStyleBackColor = true;
+            this.btAceptar.Click += new System.EventHandler(this.btAceptar_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(12, 12);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 59);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.btAceptar);
+            this.Controls.Add(this.lblIndicacion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIndicacion;
+        private System.Windows.Forms.Button btAceptar;
+        private System.Windows.Forms.TextBox txtNumero;
+    }
+}
+
---
+++ pepe/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pepe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btAceptar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int pepe = Convert.ToInt32(txtNumero.Text);
+
+                if (pepe > 10)
+                {
+                    lblIndicacion.Text = "El numero ingresado ES mayor que 10";
+                }
+                else
+                {
+                    lblIndicacion.Text = "El numero ingresado NO mayor que 10";
+                }
+            }
+            catch (Exception)
+            {
+                lblIndicacion.Text = "El dato ingresado no es correcto";
+            }
+        }
+    }
+}
